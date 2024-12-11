e9f6629762ce2aa068786381f783cad388fb1db0
Jonathon Pinnow <pinnowj@gmail.com>
2024-12-11 15:03:17 -0500

    update readme with run instructions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, env::args};
+use std::{collections::HashMap, env::args, fs};
 
 fn main() {
     // let mut input = args();
@@ -11,5 +11,21 @@ fn main() {
 }
 
 fn day_one_part_one() {
-    println!("day one part one")
+    println!("day one part one");
+    let data = retrieve_input_data("./puzzle_input/d1p1.txt");
+    let col1 = Vec::new();
+    let col2 = Vec::new();
+    for line in data.split("\n") {
+        let (first, second): (String, String) = line.split(" ").collect();
+        col1.push(first.parse::<i32>().unwrap());
+        col2.push(second.parse::<i32>().unwrap());
+    }
+    col1.sort();
+    col2.sort();
+    println!("column1: {}", col1);
+    println!("column2: {}", col2);
+}
+
+fn retrieve_input_data(path: str) -> String {
+    fs::read_to_string(path).unwrap()
 }
