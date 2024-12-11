fa69a8e8824cd71eba20ea4a90e35df48b7cec40
Jonathon Pinnow <pinnowj@gmail.com>
2024-12-11 15:31:18 -0500

    finish out day one part one solution. Passed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::{collections::HashMap, env::args, fs};
+use std::{collections::HashMap, env::args, fs, ops::IndexMut};
 
 fn main() {
-    // let mut input = args();
     println!("Advent of Code 2024!");
     let arg = args().nth(1).unwrap();
     println!("input argument: {:?}", &arg);
@@ -15,21 +14,22 @@ fn day_one_part_one() {
     let data = retrieve_input_data("./puzzle_input/d1p1.txt");
     let mut col1 = Vec::new();
     let mut col2 = Vec::new();
-    let mut n = 1;
     for line in data.split("\n") {
-        println!("line {}: {:?}", n, line);
         if line == "" {break;}
         let mut iter = line.split_whitespace();
         let first = iter.next().unwrap();
         let second = iter.next().unwrap();
         col1.push(first.parse::<i32>().unwrap());
         col2.push(second.parse::<i32>().unwrap());
-        n += 1;
     }
     col1.sort();
     col2.sort();
-    // println!("column1: {:?}", col1);
-    // println!("column2: {:?}", col2);
+
+    let mut sum = 0;
+    for (idx, id) in col1.iter().enumerate() {
+        sum += i32::abs(id - *col2.index_mut(idx));
+    }
+    println!("total summed distance between ids: {}", sum)
 }
 
 fn retrieve_input_data(path: &str) -> String {
