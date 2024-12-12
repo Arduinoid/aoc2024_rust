066c0dafb1e225e610f9891cb5c3dda6c760245d
Jonathon Pinnow <pinnowj@gmail.com>
2024-12-12 14:52:22 -0500

    add day one part two solution. Passes

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("input argument: {:?}", &arg);
     let mut map: HashMap<String, fn()> = HashMap::new();
     map.insert("d1p1".to_string(), day_one_part_one);
+    map.insert("d1p2".to_string(), day_one_part_two);
     map.get(&arg).unwrap()();
 }
 
@@ -34,4 +35,32 @@ fn day_one_part_one() {
 
 fn retrieve_input_data(path: &str) -> String {
     fs::read_to_string(path).unwrap()
+}
+
+fn day_one_part_two() {
+    println!("day one part two");
+    let data = retrieve_input_data("./puzzle_input/d1p1.txt");
+    let mut col1 = Vec::new();
+    let mut col2 = Vec::new();
+    for line in data.split("\n") {
+        if line == "" {break;}
+        let mut iter = line.split_whitespace();
+        let first = iter.next().unwrap();
+        let second = iter.next().unwrap();
+        col1.push(first.parse::<i32>().unwrap());
+        col2.push(second.parse::<i32>().unwrap());
+    }
+    col1.sort();
+    col2.sort();
+    let mut id_map = HashMap::new();
+    for i in col2 {
+        id_map.entry(i).and_modify(|v| *v += 1).or_insert(1);
+    }
+    let mut sum = 0;
+    for i in col1 {
+        if let Some(num) = id_map.get(&i) {
+            sum += i*num;
+        }
+    }
+    println!("multiplied and summed numbers from list one that were in list two: {}", sum)
 }
